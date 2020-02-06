df01800d066e9f301c6950ff8e016039f397f260
nbmateus <mateusnicolasblas@gmail.com>
2020-02-06 17:24:55 -0300

    detalles

--- statics/js/todo.js
+++ statics/js/todo.js
@@ -19,14 +19,14 @@ function getTodoList() {
                 html = "<div class='card'><div class='card-body'><h5 class='card-title'>To-do list is empty.</h5></div></div>";
             } else {
                 for (var i = 0; i < response.length; i++) {
-                    html += "<div class='card' style='width: 18rem;'><div class='card-body'><h5 class='card-title'>" + response[i].fields.todo + "</h5><h6 class='card-subtitle mb-2 text-muted'>" + response[i].fields.date + "</h6>";
+                    html += "<div class='card' style='width: 25rem;'><div class='card-body'><h5 class='card-title' align='left'>" + response[i].fields.todo + "</h5><h6 class='card-subtitle mb-2 text-muted' align='left'>" + response[i].fields.date + "</h6>";
                     if (response[i].fields.done) {
                         html += "<input class='form-check-input' type='checkbox' value='1' id='defaultCheck1' onclick='updateTodo(false," + response[i].pk + ")' checked></input>";
                     }
                     else {
                         html += "<input class='form-check-input' type='checkbox' value='1' id='defaultCheck1' onclick='updateTodo(true," + response[i].pk + ")'>";
                     }
-                    html += "<label class='form-check-label' for='defaultCheck1'>Done</label><button class='btn btn-primary' id='dlt' onclick='deleteTodo(" + response[i].pk + ")' type='submit'>Delete</button></div></div>";
+                    html += "<label class='form-check-label' for='defaultCheck1'>Done</label><button class='btn btn-primary' align='right' id='dlt' onclick='deleteTodo(" + response[i].pk + ")' type='submit'>Delete</button></div></div>";
                 }
             }
             divTodoList.innerHTML = html;
@@ -66,7 +66,8 @@ function addTodo() {
         success: function (response) {
             console.log("addTodo Succes: " + response);
             getTodoList();
-
+            document.getElementById("idNewTodo").value = "";
+            document.getElementById('idCheckboxDone').checked = false;
         },
         error: function (response) {
             console.log("addTodo Error: " + response)
